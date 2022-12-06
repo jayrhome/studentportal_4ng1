398c94ab29b70b41928ffacaa7be883db6a29293
juancarlo25 <juan.carlo.asiado.oreto@gmail.com>
2022-12-06 19:39:53 +0800

    hex

--- studentportal_4ng1/settings.py
+++ studentportal_4ng1/settings.py
@@ -125,8 +125,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'aaaa3@gmail.com'
-EMAIL_HOST_PASSWORD = 'asd'  # App password
+EMAIL_HOST_USER = 's@gmail.com'
+EMAIL_HOST_PASSWORD = 'asdasdasd'  # App password
 
 
 PASSWORD_RESET_TIMEOUT = 180  # 20 Minutes
