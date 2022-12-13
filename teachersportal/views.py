841a218b77129a03e1718feced280b1aecad0f4c
Tantan1939 <101438750+Tantan1939@users.noreply.github.com>
2022-12-13 12:14:21 +0800

    with teachersportal html files for first defense

--- teachersportal/views.py
+++ teachersportal/views.py
@@ -14,3 +14,28 @@ def teachers_only(user):
 @method_decorator([login_required(login_url="studentportal:login"), user_passes_test(teachers_only, login_url="studentportal:index")], name="dispatch")
 class index(TemplateView):
     template_name = "teachersportal/index.html"
+
+
+@method_decorator([login_required(login_url="studentportal:login"), user_passes_test(teachers_only, login_url="studentportal:index")], name="dispatch")
+class class_list(TemplateView):
+    template_name = "teachersportal/classlist.html"
+
+
+@method_decorator([login_required(login_url="studentportal:login"), user_passes_test(teachers_only, login_url="studentportal:index")], name="dispatch")
+class class_record(TemplateView):
+    template_name = "teachersportal/classrecord.html"
+
+
+@method_decorator([login_required(login_url="studentportal:login"), user_passes_test(teachers_only, login_url="studentportal:index")], name="dispatch")
+class class_assessments(TemplateView):
+    template_name = "teachersportal/class_assessments.html"
+
+
+@method_decorator([login_required(login_url="studentportal:login"), user_passes_test(teachers_only, login_url="studentportal:index")], name="dispatch")
+class class_learningmaterials(TemplateView):
+    template_name = "teachersportal/learning_materials.html"
+
+
+@method_decorator([login_required(login_url="studentportal:login"), user_passes_test(teachers_only, login_url="studentportal:index")], name="dispatch")
+class class_chatbox(TemplateView):
+    template_name = "teachersportal/groupchatbox.html"
