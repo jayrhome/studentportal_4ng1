bd0d5d66497050286a1ccfe6e747d636d8f90caf
Tantan1939 <101438750+Tantan1939@users.noreply.github.com>
2023-02-06 21:26:56 +0800

    remove one main features

--- adminportal/views.py
+++ adminportal/views.py
@@ -16,8 +16,6 @@ from django.contrib import messages
 from django.db.models import Q, FilteredRelation, Prefetch, Count, Case, When, Value
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
-from django.core.exceptions import ObjectDoesNotExist
-import re
 
 
 def superuser_only(user):
