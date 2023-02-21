0ba3a428fe1b5f5b77eb24b0f4130eff5725c039
Tantan1939 <101438750+Tantan1939@users.noreply.github.com>
2023-02-21 23:43:57 +0800

    With admission models and forms

--- registrarportal/models.py
+++ registrarportal/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from datetime import date
+from django.conf import settings
+# from adminportal.models import curriculum as cur, shs_strand as shss
+from django.utils.translation import gettext_lazy as _
 
 
 def add_school_year(start_year, year):
@@ -27,7 +30,7 @@ class schoolYear(models.Model):
 
 class enrollment_admission_setup(models.Model):
     ea_setup_sy = models.OneToOneField(
-        schoolYear, on_delete=models.PROTECT, related_name="e_a_setup")
+        schoolYear, on_delete=models.RESTRICT, related_name="e_a_setup")
     start_date = models.DateField()
     end_date = models.DateField()
     students_perBatch = models.IntegerField()
@@ -40,3 +43,152 @@ class enrollment_admission_setup(models.Model):
 
     def __str__(self):
         return f"{self.ea_setup_sy.display_sy()}: {self.start_date} - {self.end_date}"
+
+
+class student_admission_details(models.Model):
+
+    class SexChoices(models.TextChoices):
+        MALE = 'M', _('Male')
+        FEMALE = 'F', _('Female')
+
+    admission_owner = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, related_name="admission_details")
+    first_name = models.CharField(max_length=20)
+    middle_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    sex = models.CharField(max_length=2, choices=SexChoices.choices)
+    date_of_birth = models.DateField()
+    birthplace = models.CharField(max_length=200)
+    nationality = models.CharField(max_length=50)
+
+    # Elementary school details
+    elem_name = models.CharField(max_length=50)
+    elem_address = models.CharField(max_length=50)
+    elem_region = models.CharField(max_length=30)
+    elem_year_completed = models.DateField()
+    elem_pept_passer = models.BooleanField(default=False)
+    elem_pept_date_completion = models.DateField(null=True, blank=True)
+    elem_ae_passer = models.BooleanField(default=False)
+    elem_ae_date_completion = models.DateField(null=True, blank=True)
+    elem_community_learning_center = models.CharField(
+        max_length=50, null=True, blank=True)
+    elem_clc_address = models.CharField(max_length=50, null=True, blank=True)
+
+    # Junior High school details
+    jhs_name = models.CharField(max_length=50)
+    jhs_address = models.CharField(max_length=50)
+    jhs_region = models.CharField(max_length=30)
+    jhs_year_completed = models.DateField()
+    jhs_pept_passer = models.BooleanField(default=False)
+    jhs_pept_date_completion = models.DateField(null=True, blank=True)
+    jhs_ae_passer = models.BooleanField(default=False)
+    jhs_ae_date_completion = models.DateField(null=True, blank=True)
+    jhs_community_learning_center = models.CharField(
+        max_length=50, null=True, blank=True)
+    jhs_clc_address = models.CharField(max_length=50, null=True, blank=True)
+
+    is_accepted = models.BooleanField(default=False)  # if pending or accepted
+    admission_sy = models.ForeignKey(
+        schoolYear, on_delete=models.RESTRICT, related_name="sy_admitted")
+    first_chosen_strand = models.ForeignKey(
+        "adminportal.shs_strand", on_delete=models.RESTRICT, related_name="first_strand")
+    second_chosen_strand = models.ForeignKey(
+        "adminportal.shs_strand", on_delete=models.RESTRICT, related_name="second_strand")
+    assigned_curriculum = models.ForeignKey(
+        "adminportal.curriculum", on_delete=models.RESTRICT, related_name="with_admission")
+    batch_number = models.IntegerField()
+
+    foreign_citizen = models.BooleanField(default=False)
+    dual_citizen = models.BooleanField(default=False)
+
+    is_denied = models.BooleanField(default=False)  # if denied, for review
+    modified_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-admission_sy__id", "created_on"]
+        get_latest_by = ["created_on"]
+
+    def __str__(self):
+        return f"{self.id}: {self.first_name} {self.middle_name} {self.last_name}"
+
+    def elementary_school(self):
+        return self.elem_name
+
+    def jhs(self):
+        return self.jhs_name
+
+    # def to_pendingList(self):
+    #     return reverse("adminportal:admission")
+
+    # def to_admittedList(self):
+    #     return reverse("adminportal:admitted_students")
+
+    # def to_reviewList(self):
+    #     return reverse("adminportal:forReviewAdmission")
+
+    # def to_deniedList(self):
+    #     return reverse("adminportal:denied_admissions")
+
+    # def to_holdList(self):
+    #     return reverse("adminportal:hold_admissions")
+
+
+class admission_requirements(models.Model):
+    good_moral = models.ImageField(upload_to="admission/goodMorals/%Y")
+    report_card = models.ImageField(upload_to="admission/reportCards/%Y")
+    psa = models.ImageField(upload_to="admission/studentPsa/%Y")
+
+    class Meta:
+        abstract = True
+
+
+class ph_born(admission_requirements):
+    admission = models.ForeignKey(
+        student_admission_details, on_delete=models.RESTRICT, related_name="softCopy_admissionRequirements_phBorn")
+    modified_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.id
+
+
+class foreign_citizen_documents(admission_requirements):
+    admission = models.ForeignKey(
+        student_admission_details, on_delete=models.RESTRICT, related_name="softCopy_admissionRequirements_foreigner")
+    alien_certificate_of_registration = models.ImageField(
+        upload_to="admission/foreignCitizenDocuments/alienRegistration/%Y")
+    study_permit = models.ImageField(
+        upload_to="admission/foreignCitizenDocuments/studyPermit/%Y")
+    f137 = models.ImageField(
+        upload_to="admission/foreignCitizenDocuments/f137s/%Y")
+    modified_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.id
+
+
+class dual_citizen_documents(admission_requirements):
+    admission = models.ForeignKey(
+        student_admission_details, on_delete=models.RESTRICT, related_name="softCopy_admissionRequirements_dualCitizen")
+    dual_citizenship = models.ImageField(
+        upload_to="admission/dualCitizenDocuments/dualCitizenshipCertificates/%Y")
+    philippine_passport = models.ImageField(
+        upload_to="admission/dualCitizenDocuments/phPassports/%Y")
+    f137 = models.ImageField(
+        upload_to="admission/dualCitizenDocuments/f137s/%Y")
+    modified_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.id
--- studentportal/forms.py
+++ studentportal/forms.py
@@ -3,6 +3,8 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 import re
 from adminportal.models import *
+from registrarportal.models import *
+from usersPortal.forms import validate_imageSize
 
 User = get_user_model()
 
@@ -53,70 +55,89 @@ def validate_schedule(dt):
         raise ValidationError("Invalid Date.")
 
 
-# class admission_personal_details(forms.Form):
-#     first_name = forms.CharField(label="First Name", max_length=20)
-#     middle_name = forms.CharField(label="Middle Name", max_length=20)
-#     last_name = forms.CharField(label="Last Name", max_length=20)
-#     sex = forms.ChoiceField(
-#         label="Sex", choices=student_admission_details.SexChoices.choices)
-#     birth_date = forms.DateField(label="Birthdate", validators=[
-#                                  birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}))
-#     birthplace = forms.CharField(label="Place of birth", max_length=200)
-#     nationality = forms.CharField(label="Nationality", max_length=50)
-#     # first_chosen_strand = forms.ChoiceField(label="Select First Strand", choices=(
-#     #     (strand.id, strand.track.track_name + " - " + strand.strand_name) for strand in shs_strand.objects.select_related('track').exclude(is_deleted=True)
-#     # ))
-#     # second_chosen_strand = forms.ChoiceField(label="Select Second Strand", choices=(
-#     #     (strand.id, strand.track.track_name + " - " + strand.strand_name) for strand in shs_strand.objects.select_related('track').exclude(is_deleted=True)
-#     # ))
+class admission_personal_details(forms.Form):
+    first_name = forms.CharField(label="First Name", max_length=20)
+    middle_name = forms.CharField(label="Middle Name", max_length=20)
+    last_name = forms.CharField(label="Last Name", max_length=20)
+    sex = forms.ChoiceField(
+        label="Sex", choices=student_admission_details.SexChoices.choices)
+    birth_date = forms.DateField(label="Birthdate", validators=[
+                                 birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}))
+    birthplace = forms.CharField(label="Place of birth", max_length=200)
+    nationality = forms.CharField(label="Nationality", max_length=50)
+    first_chosen_strand = forms.TypedChoiceField(label="Choose First Strand", choices=(
+        (strand.strand.id, f"{strand.strand.track.track_name}: {strand.strand.strand_name}") for strand in curriculum.objects.order_by('strand').distinct('strand')), coerce=str)
+    second_chosen_strand = forms.TypedChoiceField(label="Choose Second Strand", choices=(
+        (strand.strand.id, f"{strand.strand.track.track_name}: {strand.strand.strand_name}") for strand in curriculum.objects.order_by('strand').distinct('strand')), coerce=str)
 
 
-# class elementary_school_details(forms.Form):
-#     elem_name = forms.CharField(label="Elementary School Name", max_length=50)
-#     elem_address = forms.CharField(
-#         label="Elementary School Address", max_length=50)
-#     elem_region = forms.CharField(label="School Region", max_length=30)
-#     elem_year_completed = forms.DateField(label="Year Completed", validators=[
-#                                           birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}))
-
-#     elem_pept_passer = forms.ChoiceField(
-#         label="Are you a passer of Philippine Educational Placement Test (PEPT) for Elementary Level?", choices=boolean_choices())
-#     elem_pept_date_completion = forms.DateField(label="Date Completed", validators=[
-#                                                 birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}), required=False, help_text="Enter date completion if PEPT passer")
-
-#     elem_ae_passer = forms.ChoiceField(
-#         label="Are you a passer of Accreditation and Equivalency (A&E) Test for Elementary Level?", choices=boolean_choices())
-#     elem_ae_date_completion = forms.DateField(label="Date Completed", validators=[
-#                                               birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}), required=False, help_text="Enter date completion if A&E passer")
-
-#     elem_community_learning_center = forms.CharField(
-#         label="Name of Community Learning Center", max_length=50, required=False, help_text="If applicable")
-#     elem_clc_address = forms.CharField(
-#         label="Community Learning Center Address", max_length=50, required=False, help_text="If applicable")
+class elementary_school_details(forms.Form):
+    elem_name = forms.CharField(label="Elementary School Name", max_length=50)
+    elem_address = forms.CharField(
+        label="Elementary School Address", max_length=50)
+    elem_region = forms.CharField(label="School Region", max_length=30)
+    elem_year_completed = forms.DateField(label="Year Completed", validators=[
+                                          birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}))
+    elem_pept_passer = forms.ChoiceField(
+        label="Are you a passer of Philippine Educational Placement Test (PEPT) for Elementary Level?", choices=boolean_choices())
+    elem_pept_date_completion = forms.DateField(label="Date Completed", validators=[
+                                                birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}), required=False, help_text="Enter date completion if PEPT passer")
+    elem_ae_passer = forms.ChoiceField(
+        label="Are you a passer of Accreditation and Equivalency (A&E) Test for Elementary Level?", choices=boolean_choices())
+    elem_ae_date_completion = forms.DateField(label="Date Completed", validators=[
+                                              birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}), required=False, help_text="Enter date completion if A&E passer")
+    elem_community_learning_center = forms.CharField(
+        label="Name of Community Learning Center", max_length=50, required=False, help_text="If applicable")
+    elem_clc_address = forms.CharField(
+        label="Community Learning Center Address", max_length=50, required=False, help_text="If applicable")
 
 
-# class jhs_details(forms.Form):
-#     jhs_name = forms.CharField(label="Junior High School Name", max_length=50)
-#     jhs_address = forms.CharField(
-#         label="Junior High School Address", max_length=50)
-#     jhs_region = forms.CharField(label="School Region", max_length=30)
-#     jhs_year_completed = forms.DateField(label="Year Completed", validators=[
-#         birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}))
+class jhs_details(forms.Form):
+    jhs_name = forms.CharField(label="Junior High School Name", max_length=50)
+    jhs_address = forms.CharField(
+        label="Junior High School Address", max_length=50)
+    jhs_region = forms.CharField(label="School Region", max_length=30)
+    jhs_year_completed = forms.DateField(label="Year Completed", validators=[
+        birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}))
+    jhs_pept_passer = forms.ChoiceField(
+        label="Are you a passer of Philippine Educational Placement Test (PEPT) for JHS Level?", choices=boolean_choices())
+    jhs_pept_date_completion = forms.DateField(label="Date Completed", validators=[
+                                               birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}), required=False, help_text="Enter date completion if PEPT passer")
+    jhs_ae_passer = forms.ChoiceField(
+        label="Are you a passer of Accreditation and Equivalency (A&E) Test for JHS Level?", choices=boolean_choices())
+    jhs_ae_date_completion = forms.DateField(label="Date Completed", validators=[
+                                             birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}), required=False, help_text="Enter date completion if A&E passer")
+    jhs_community_learning_center = forms.CharField(
+        label="Name of Community Learning Center", max_length=50, required=False, help_text="If applicable")
+    jhs_clc_address = forms.CharField(
+        label="Community Learning Center Address", max_length=50, required=False, help_text="If applicable")
 
-#     jhs_pept_passer = forms.ChoiceField(
-#         label="Are you a passer of Philippine Educational Placement Test (PEPT) for JHS Level?", choices=boolean_choices())
-#     jhs_pept_date_completion = forms.DateField(label="Date Completed", validators=[
-#                                                birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}), required=False, help_text="Enter date completion if PEPT passer")
 
-#     jhs_ae_passer = forms.ChoiceField(
-#         label="Are you a passer of Accreditation and Equivalency (A&E) Test for JHS Level?", choices=boolean_choices())
-#     jhs_ae_date_completion = forms.DateField(label="Date Completed", validators=[
-#                                              birthdate_validator], widget=forms.DateInput(attrs={'type': 'date'}), required=False, help_text="Enter date completion if A&E passer")
+class admissionRequirementsForm(forms.Form):
+    good_moral = forms.ImageField(label="Good Moral Certificate", widget=forms.ClearableFileInput(
+        attrs={"style": "display:none;"}), validators=[validate_imageSize, ])
+    report_card = forms.ImageField(label="Report Card", widget=forms.ClearableFileInput(
+        attrs={"style": "display:none;"}), validators=[validate_imageSize, ])
+    psa = forms.ImageField(label="Philippine Birth Certificate", widget=forms.ClearableFileInput(
+        attrs={"style": "display:none;"}), validators=[validate_imageSize, ])
 
-#     jhs_community_learning_center = forms.CharField(
-#         label="Name of Community Learning Center", max_length=50, required=False, help_text="If applicable")
-#     jhs_clc_address = forms.CharField(
-#         label="Community Learning Center Address", max_length=50, required=False, help_text="If applicable")
+
+class foreignApplicantForm(admissionRequirementsForm):
+    alien_certificate_of_registration = forms.ImageField(label="Alien Certificate of Registration", widget=forms.ClearableFileInput(
+        attrs={"style": "display:none;"}), validators=[validate_imageSize, ])
+    study_permit = forms.ImageField(label="Study Permit", widget=forms.ClearableFileInput(
+        attrs={"style": "display:none;"}), validators=[validate_imageSize, ])
+    f137 = forms.ImageField(label="School Permanent Record (F137)", widget=forms.ClearableFileInput(
+        attrs={"style": "display:none;"}), validators=[validate_imageSize, ])
+
+
+class dualCitizenApplicantForm(admissionRequirementsForm):
+    dual_citizenship = forms.ImageField(label="Certificate of Dual Citizenship", widget=forms.ClearableFileInput(
+        attrs={"style": "display:none;"}), validators=[validate_imageSize, ])
+    philippine_passport = forms.ImageField(label="Philippine Passport", widget=forms.ClearableFileInput(
+        attrs={"style": "display:none;"}), validators=[validate_imageSize, ])
+    f137 = forms.ImageField(label="School Permanent Record (F137)", widget=forms.ClearableFileInput(
+        attrs={"style": "display:none;"}), validators=[validate_imageSize, ])
 
 
 # class enrollment_form(forms.Form):
