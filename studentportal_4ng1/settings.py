a1cd1a365e1184aee3a38b821e66968b4f09eced
juancarlo25 <juan.carlo.asiado.oreto@gmail.com>
2023-02-26 17:01:33 +0800

    admin

--- studentportal_4ng1/settings.py
+++ studentportal_4ng1/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db4ng1',
         'USER': 'postgres',
-        'PASSWORD': 'SIGMA2022',
+        'PASSWORD': 'Sniper09',
         'HOST': 'localhost'
     }
 }
